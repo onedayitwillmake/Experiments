38fd1f01ba6fce40adab86116da176edac71e081
Mario Gonzalez <mariogonzalez@gmail.com>
2012-07-22 18:17:43 -0400

    Modified ribbonPaintCanvas to support iOS devices

--- MyIadProject/MyAdProject.iadproj/pages/C8643582-4FDD-41AF-8C41-6747B6777813$phoneFieldDelegate.js
+++
@@ -1,9 +0,0 @@
-
-this.onViewTouchUpInside = function (event) { 
-	 // strip out formatting from the number on this button 
-	 var phoneNumber = parseInt(this.viewController.outlets.phone.text.match(/\d/g).join(''), 10);  
- 
-	 // dial that number 
-	 window.location = 'tel://' + phoneNumber; 
-}; 
-
