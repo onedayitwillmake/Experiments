dca3697f2cde56890e908e4eb9e72bea1b264a71
Mario Gonzalez <mariogonzalez@gmail.com>
2012-09-28 12:29:08 -0400

    algorithmsjs/binarysearch.js

---
+++ algorithmsjs/binarysearch.js
@@ -0,0 +1,16 @@
+var binarySearch = function( key, theArray ) {
+	var lo = 0;
+	var hi = theArray.length - 1;
+	while( lo <= hi ) {
+		var mid = Math.floor( lo + (hi-lo) / 2 );
+		if( key < theArray[mid] ) hi = mid - 1;
+		else if ( key > theArray[mid] ) lo = mid + 1
+		else return mid;
+	}
+}
+
+var theArray = [];
+for(var i = 0; i < 100; ++i ) 
+	theArray.push( i );
+
+console.log( binarySearch(45, theArray) );
